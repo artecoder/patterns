0da66a8c1de9a9e2361f18e1428f33f50406e746
Rodolfo Arteaga Held <Rarteaga@intergrupo.com>
2022-03-23 21:57:28 -0500

    change class name

---
+++ gof.creational.BuilderPattern/CarBuilder/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBuilder
+{
+    public class Car
+    {
+        public string Chasis { get; set; }
+        public string Engine { get; set; }
+        public string Bodywork { get; set; }
+        public string[] Tires { get; set; }
+
+    }
+}
--- gof.creational.BuilderPattern/CarBuilder/CarBuilder.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CarBuilder
-{
-    public class CarBuilder
-    {
-
-    }
-}
